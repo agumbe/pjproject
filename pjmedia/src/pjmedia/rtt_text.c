140ea42a84098ae9f46da8e2a5342d9987ff3365
tarun <agumbe22@gmail.com>
2021-03-19 16:32:51 +0530

    RTT logging

--- pjmedia/src/pjmedia/rtt_text.c
+++ pjmedia/src/pjmedia/rtt_text.c
@@ -182,7 +182,7 @@ PJ_DECL(pj_status_t) pjmedia_text_stream_start(pjmedia_rtt_stream* text_stream)
 {
         pj_status_t status;
 
-        PJ_LOG(1, (THIS_FILE, "inside pjmedia_text_stream_start\n"));
+        PJ_LOG(1, (THIS_FILE, "inside pjmedia_text_stream_start 1 \n"));
         /* If this is a mid-call media update, then destroy existing media */
         if (text_stream->thread != NULL)
                 destroy_call_media(text_stream);
